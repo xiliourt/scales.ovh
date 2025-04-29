9d5e2da03dd49b435ba62726231a78b5829487be
xiliourt <dyl.sec.ure@gmail.com>
2025-04-29 17:13:41 +1000

    Create disclaimer.php

---
+++ disclaimer.php
@@ -0,0 +1,6 @@
+<div class="disclaimer">
+  <strong>IMPORTANT DISCLAIMER</strong>
+    This screening tool is for informational purposes only and is NOT a substitute for a professional diagnosis. A score
+    indicating symptoms requires evaluation by a qualified mental health professional. Please consult a doctor or mental
+    health provider if you have concerns about your mental health.
+</div>
